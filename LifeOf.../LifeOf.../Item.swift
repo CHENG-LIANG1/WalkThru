1e53f7657a27bd456443c3eb001777ed7d7b4676
Ray <ray@RaydeMac-mini.local>
2026-05-22 23:04:06 +0800

    feat: initialize LifeOf SwiftUI iOS project

---
+++ LifeOf.../LifeOf.../Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LifeOf...
+//
+//  Created by Ray on 2026/5/22.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
